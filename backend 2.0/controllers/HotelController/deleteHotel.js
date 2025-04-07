7dedfe3685e14ecf6a87cc3197176276829b9943
Arnav Chhabra <65032719+Halleys123@users.noreply.github.com>
2025-04-08 02:03:28 +0530

    feat: delete hotels complete

--- backend 2.0/controllers/HotelController/deleteHotel.js
+++ backend 2.0/controllers/HotelController/deleteHotel.js
@@ -2,10 +2,14 @@ const HotelSchema = require('../../schemas/HotelSchema');
 const catchAsync = require('../../utils/catchAsync');
 const sendResponse = require('../../utils/sendResponse');
 
-const deleteAllHotel = catchAsync(async (req, res, next) => {});
-const deleteHotel = catchAsync(async (req, res, next) => {});
+const deleteAllHotel = catchAsync(async (req, res, next) => {
+  sendResponse(res, 200, true, 'Delete all', {});
+});
+const deleteHotel = catchAsync(async (req, res, next) => {
+  sendResponse(res, 200, true, 'Delete one by id', {});
+});
 
-module.export = {
+module.exports = {
   deleteAllHotel,
   deleteHotel,
 };
